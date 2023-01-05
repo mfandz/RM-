037cfce9057aa704d828461dcbefb4d55c8848bf
mfandz <103648029+mfandz@users.noreply.github.com>
2023-01-05 22:33:52 +0800

    Add files via upload

---
+++ .vs/install/video_interfaces/share/video_interfaces/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(video_interfaces_IDL_FILES "msg/Pos.idl;msg/Send.idl;msg/Kmf.idl;msg/Kmfs.idl")
+set(video_interfaces_INTERFACE_FILES "msg/Pos.msg;msg/Send.msg;msg/Kmf.msg;msg/Kmfs.msg")
---
+++ .vs/install/video_interfaces/share/video_interfaces/cmake/video_interfaces__rosidl_typesupport_introspection_cppExport-noconfig.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file.
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "video_interfaces::video_interfaces__rosidl_typesupport_introspection_cpp" for configuration ""
+set_property(TARGET video_interfaces::video_interfaces__rosidl_typesupport_introspection_cpp APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(video_interfaces::video_interfaces__rosidl_typesupport_introspection_cpp PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libvideo_interfaces__rosidl_typesupport_introspection_cpp.so"
+  IMPORTED_SONAME_NOCONFIG "libvideo_interfaces__rosidl_typesupport_introspection_cpp.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS video_interfaces::video_interfaces__rosidl_typesupport_introspection_cpp )
+list(APPEND _IMPORT_CHECK_FILES_FOR_video_interfaces::video_interfaces__rosidl_typesupport_introspection_cpp "${_IMPORT_PREFIX}/lib/libvideo_interfaces__rosidl_typesupport_introspection_cpp.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
